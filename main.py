838cb748ae1f40e15a16f6fab78c3c08c3296764
shiv <Shivansh.Sharma859@education.nsw.gov.au>
2026-06-29 23:54:36 +0000

    established all the rooms and made their links accurate

--- main.py
+++ main.py
@@ -3,30 +3,115 @@ from cave import Classroom
 
 dead = False
 
-# Create caves
-cavern = Classroom("Cavern")
-grotto = Classroom("Grotto")
-dungeon = Classroom("Dungeon")
+# Create rooms
+
+#BASIC ROOMS
+math_room = Classroom("Mathematics")
+english_room = Classroom("English")
+science_room = Classroom("Science")
+hall = Classroom("Hall")
+entrance = Classroom("Entrance")
+hall = Classroom("Hall")
+principal_office = Classroom("Principal's Office")
+exit_room = Classroom("Exit")
+
+#Non-classroom rooms
+cafe = Classroom("Cafe")
+canteen = Classroom("Canteen")
+library = Classroom("Library")
+
+#Science labs & computer lab 
+computer_lab = Classroom("Computer Lab")
+biology = Classroom("Biology")
+physics = Classroom("Physics")
+chemistry = Classroom("Chemistry")
+
+#Maths Wing (Upper Level)
+mathematics = Classroom("Mathematics")
+m_faculty1 = Classroom("M. Faculty 1")
+m_faculty2 = Classroom("M. Faculty 2")
+
+#English Wing (Lower Level)
+english = Classroom("English")
+hsie = Classroom("HSIE")
+
+#Pe Wing (outdoors)
+pe1 = Classroom("PE 1")
+pe2 = Classroom("PE 2")
+
+#Other rooms (misc.)
+oval = Classroom("Oval")
+detention = Classroom("Detention")
+
 
 # Descriptions
-cavern.set_description("A dark and dirty cavern")
-grotto.set_description("A small cave with ancient markings")
-dungeon.set_description("A large cave with a rack")
+hall.set_description("Hall - Students everywhere.")
+math_room.set_description("Math room-  on the blackboards.")
+english_room.set_description("A small cave with ancient markings")
+science_room.set_description("A large cave with a rack")
+
 
 # Links
-cavern.set_link_classroom(dungeon, "south")
-dungeon.set_link_classroom(cavern, "north")
-dungeon.set_link_classroom(grotto, "west")
-grotto.set_link_classroom(dungeon, "east")
+math_room.set_link_classroom(math_room, "south")
+english_room.set_link_classroom(english_room, "north")
+science_room.set_link_classroom(science_room, "west")
+science_room.set_link_classroom(math_room, "east")
+entrance.set_link_classroom(hall, "north")
+hall.set_link_classroom(entrance, "south")
+hall.set_link_classroom(principal_office, "north")
+hall.set_link_classroom(cafe, "west")
+hall.set_link_classroom(canteen, "east")
+hall.set_link_classroom(library, "south")
+principal_office.set_link_classroom(exit_room, "north")
+exit_room.set_link_classroom(principal_office, "south")
+hall.set_link_classroom(computer_lab, "northeast")
+computer_lab.set_link_classroom(hall, "southwest")
+
+#Science Wing
+computer_lab.set_link_classroom(biology, "east")
+biology.set_link_classroom(computer_lab, "west")
+
+biology.set_link_classroom(physics, "south")
+physics.set_link_classroom(biology, "north")
+
+physics.set_link_classroom(chemistry, "west")
+chemistry.set_link_classroom(physics, "east")
+
+hall.set_link_classroom(mathematics, "northwest")
+mathematics.set_link_classroom(hall, "southeast")
+
+mathematics.set_link_classroom(m_faculty1, "west")
+m_faculty1.set_link_classroom(mathematics, "east")
+
+m_faculty1.set_link_classroom(m_faculty2, "south")
+m_faculty2.set_link_classroom(m_faculty1, "north")
+
+hall.set_link_classroom(english, "southeast")
+english.set_link_classroom(hall, "northwest")
+
+english.set_link_classroom(hsie, "south")
+hsie.set_link_classroom(english, "north")
+
+hsie.set_link_classroom(pe1, "east")
+pe1.set_link_classroom(hsie, "west")
+
+pe1.set_link_classroom(pe2, "south")
+pe2.set_link_classroom(pe1, "north")
+
+library.set_link_classroom(oval, "south")
+oval.set_link_classroom(library, "north")
+
+oval.set_link_classroom(detention, "east")
+detention.set_link_classroom(oval, "west")
 
 # Enemy
-harry = Teacher("Harry", "A hairy, smelly wumpus")
-dungeon.set_character(harry)
+principal = Teacher("Prin C. Pal", "The principal of the school")
+math_room.set_character(principal)
 
-harry.set_conversation("Come closer... I can't see you.")
-harry.set_weakness("aura")
+principal.set_conversation("Come closer... I can't see you.")
+principal.set_weakness("aura")
 
-current_cave = cavern
+current_cave = math_room
 
 while not dead:
 
